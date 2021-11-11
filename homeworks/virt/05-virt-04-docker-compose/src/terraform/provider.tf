25feda1f225cae8db96ee43319f68397251cebff
EmilTK <32601986+EmilTK@users.noreply.github.com>
2021-11-11 23:11:34 +0300

    Solving homework for lesson 5.4

--- homeworks/virt/05-virt-04-docker-compose/src/terraform/provider.tf
+++ homeworks/virt/05-virt-04-docker-compose/src/terraform/provider.tf
@@ -8,7 +8,8 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "key.json"
+  # service_account_key_file = "key.json"
+  token = "AQAAAAAXvBz0AATuwSpq6nDRZ0ndr5h6BKP9dJQ"
   cloud_id  = "${var.yandex_cloud_id}"
   folder_id = "${var.yandex_folder_id}"
 }
